6738cce7315e187a8e93ca9acb9ad653f745da59
Babouchot <bastien.maureille@hotmail.fr>
2013-04-27 17:35:25 +0200

    human update

--- Meow/src/core/Human.java
+++ Meow/src/core/Human.java
@@ -5,10 +5,16 @@ import java.util.Random;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
 
 import room.Room;
 
 public class Human {
+	
+	static final int pbedroom1 = 15;
+	static final int pbedroom2 = 30;
+	static final int pkitchen = 60;
+	static final int plounge = 100;
 	
 	private Room room;
 	private int positionX;
@@ -16,12 +22,12 @@ public class Human {
 	private Image image;
 	private Map map;
 	
-	public Human(Room room, int positionX, int positionY, Image image, Map map) {
+	public Human(Room room, int positionX, int positionY, String image, Map map) throws SlickException{
 		super();
 		this.room = room;
 		this.positionX = positionX;
 		this.positionY = positionY;
-		this.image = image;
+		//TODO this.image = new Image(image);
 		this.map = map;
 	}
 
@@ -31,8 +37,18 @@ public class Human {
 	
 	public void update() {
 		Random rand = new Random();
-		int randomInt = rand.nextInt(4);
-		room = map.getRooms().get(randomInt);
+		int randomInt = rand.nextInt(100);
+		if (randomInt <= pbedroom1)
+			room = map.getRooms().get(0);
+		else if (randomInt <= pbedroom2)
+			room = map.getRooms().get(1);
+		else if (randomInt <= pkitchen)
+			room = map.getRooms().get(2);
+		else 
+			room = map.getRooms().get(3);
+
+		positionX = room.getPositionX()+room.getWidth()/2;
+		positionY = room.getPositionY()+room.getHeight()/2;
 	}
 	
 	public Room getRoom() {
@@ -67,8 +83,9 @@ public class Human {
 
 
 	public void draw(Graphics g) {
-		g.setColor(Color.cyan);
-		g.fillRect(this.getPositionX(), this.getPositionY(), image.getWidth(), image.getHeight());
+		g.setColor(Color.gray);
+		//TODO g.fillRect(this.getPositionX(), this.getPositionY(), image.getWidth(), image.getHeight());
+		g.fillRect(this.getPositionX(), this.getPositionY(), 50,50);
 	}
 
 }
--- Meow/src/room/Room.java
+++ Meow/src/room/Room.java
@@ -13,15 +13,11 @@ public class Room {
 	private ArrayList<Object> obj;
 	private int positionX;
 	private int positionY;
-	private int width;
-	private int height;
 	private Image img;
 	
 	public Room(int x, int y, int w, int h) {
 		positionX = x;
 		positionY = y;
-		width = w;
-		height = h;
 		obj = new ArrayList<Object>();
 	}
 	
@@ -38,6 +34,7 @@ public class Room {
 	}
 	
 	public void action() {
+		// TODO
 	}
 	
 	public void draw(Graphics g){
@@ -69,19 +66,11 @@ public class Room {
 	}
 
 	public int getWidth() {
-		return width;
-	}
-
-	public void setWidth(int width) {
-		this.width = width;
+		return img.getWidth();
 	}
 
 	public int getHeight() {
-		return height;
-	}
-
-	public void setHeight(int height) {
-		this.height = height;
+		return img.getHeight();
 	}
 
 	public Image getImg() {

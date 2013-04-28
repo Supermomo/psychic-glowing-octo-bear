d2bf1a068b2246784fb42d72d43f703bab7d0e8b
Maurogik <gwenn.aubert@gmail.com>
2013-04-28 16:27:10 +0200

    r

--- Meow/src/room/Kitchen.java
+++ Meow/src/room/Kitchen.java
@@ -28,7 +28,7 @@ public class Kitchen extends Room{
 		nbHuman++;
 		Bowl b = (Bowl) getObj().get(0);
 		h.setPositionX(this.getPositionX()+this.getWidth()/2 - (nbHuman-1)*60);
-		if(timer > 60000){
+		if(timer + Human.frustration * 500 > 60000){
 			b.plein = true;
 			timer = 0;
 		}
